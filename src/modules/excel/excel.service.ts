4aeea2684435c0cb13d78298c6de5c395d5b6b40
seanghor <haiseanghor20@kit.edu.kh>
2023-08-24 05:29:35 +0700

    add json backup

--- src/modules/excel/excel.service.ts
+++ src/modules/excel/excel.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Workbook } from 'exceljs';
-import * as path from 'path';
+// import * as path from 'path';
+import path from 'path';
+
 
 interface Historic {
   id: number;
@@ -180,6 +182,66 @@ export class ExcelService {
     });
 
     const exportPath = path.resolve(__dirname, location + '.xlsx');
+    await workbook.xlsx.writeFile(exportPath);
+    return exportPath;
+  }
+
+  // -------------------------------------------------------
+  async downloadExcelAllUsers(
+    data: Historic[]
+  ): Promise<string> {
+    if (!data) {
+      throw new NotFoundException('No data found');
+    }
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet("users");
+
+    worksheet.columns = [
+      { header: 'Id', key: 'id', width: 7 },
+      { header: 'Name', key: 'name', width: 15 },
+      { header: 'Role', key: 'role', width: 7 },
+      { header: 'FaceString', key: 'faceString', width: 30 },
+      { header: 'CheckIn', key: 'checkIn', width: 10 },
+      { header: 'Checkout', key: 'checkout', width: 10 },
+      { header: 'Level', key: 'level', width: 10 },
+      { header: 'Teacher', key: 'teacher', width: 10 },
+      { header: 'FatherName', key: 'fatherName', width: 10 },
+      { header: 'FatherNumber', key: 'fatherNumber', width: 10 },
+      { header: 'FatherChatId', key: 'fatherChatId', width: 10 },
+      { header: 'MotherName', key: 'motherName', width: 10 },
+      { header: 'MotherNumber', key: 'motherNumber', width: 10 },
+      { header: 'MotherChatId', key: 'motherChatId', width: 10 },
+      { header: 'LearningShift', key: 'learningShift', width: 10 },
+      { header: 'CreatedAt', key: 'createdAt', width: 10 },
+
+    ];
+
+    for (const item of data) {
+      worksheet.addRow({
+        ...item,
+      });
+    }
+
+    const headerRow = worksheet.getRow(1);
+    headerRow.eachCell((cell) => {
+      cell.border = {
+        top: { style: 'thin' },
+        left: { style: 'thin' },
+        bottom: { style: 'thin' },
+        right: { style: 'thin' },
+      };
+      cell.fill = {
+        type: 'pattern',
+        pattern: 'solid',
+        fgColor: { argb: 'FFFFC000' },
+      };
+      cell.font = { bold: true };
+      cell.alignment = { horizontal: 'center' };
+    });
+
+    const exportPath = path.resolve(__dirname,  'user.xlsx');
+    console.log("exportPath:",exportPath);
+    
     await workbook.xlsx.writeFile(exportPath);
     return exportPath;
   }
---
+++ src/modules/test/test.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTestDto } from './dto/create-test.dto';
+import { UpdateTestDto } from './dto/update-test.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ExcelService } from '../excel/excel.service';
+
+@Injectable()
+export class TestService {
+  constructor(
+    private prisma: PrismaService,
+    private excel: ExcelService,
+  ) { }
+  create(createTestDto: CreateTestDto) {
+    return 'This action adds a new test';
+  }
+
+  async exportAllUser() {
+    const data = []
+    const users = await this.prisma.users.findMany();
+    for (const user of users) {
+      data.push({
+        id: user.id,
+        name: user.name,
+        role: "STUDENT",
+        faceString: user.faceString,
+        checkIn: user.checkIn,
+        checkOut: user.checkOut,
+        level: "string",
+        teacher: null,
+        fatherName: null,
+        fatherNumber: null,
+        fatherChatId: null,
+        motherName: null,
+        motherNumber: null,
+        motherChatId: null,
+        learningShift: null,
+        createAt: user.createdAt
+      })
+    }
+    // const res = await this.excel.downloadExcelAllUsers(data)
+    // console.log(res);
+    return data
+  }
+
+  async exportAllAttendance() {
+    const data = []
+    const users = await this.prisma.attendances.findMany();
+    // for (const user of users) {
+    //   data.push({
+    //     id: user.id,
+    //     date: user.date,
+    //     level: "string",
+    //     checkIn: "--:--",
+    //     checkOut: "--:--",
+    //     temperature: null,
+    //     attendanceStatus: null,
+    //     checkOutStatus: null,
+    //     userId: null,
+    //     name: null,
+    //   })
+    // }
+    // const res = await this.excel.downloadExcelAllUsers(data)
+    // console.log(res);
+    return users
+  }
+
+  async exportAllHistAttendance() {
+    const hists = await this.prisma.historicAtt.findMany();
+    return hists
+  }
+  async exportAllAttendanceRule() {
+    const hists = await this.prisma.attendanceRule.findMany();
+    return hists
+  }
+
+  async exportAllLocation() {
+    const hists = await this.prisma.location.findMany();
+    return hists
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} test`;
+  }
+
+  update(id: number, updateTestDto: UpdateTestDto) {
+    return `This action updates a #${id} test`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} test`;
+  }
+}
